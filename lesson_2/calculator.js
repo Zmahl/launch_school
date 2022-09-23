864fc51511da66e75756e1b4b4e8265f7dd3558b
Zmahl <zachmahl@udel.edu>
2022-09-23 16:31:15 -0400

    started lesson2

---
+++ lesson_2/calculator.js
@@ -0,0 +1,11 @@
+//The require is is used as a library import, points to library in
+//node_modules folder
+const readline = require('readline-sync');
+
+console.log('Welcome to Calculator!');
+
+console.log("What's the first number?"); 
+let number1 = readline.question();
+
+console.log(number1);
+
