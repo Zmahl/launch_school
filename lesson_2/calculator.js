a1b857c0cf238baad886776fee20df843cea683b
Zmahl <zachmahl@udel.edu>
2022-09-28 12:57:12 -0400

    finished calc and debug

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -2,37 +2,58 @@
 //node_modules folder
 const readline = require('readline-sync');
 
-console.log('Welcome to Calculator!');
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
 
-console.log("What's the first number?"); 
+function invalidNumber(number) {
+  return number.trimStart() === '' || Number.isNaN(Number(number));
+}
+
+prompt('Welcome to Calculator!');
+
+prompt("What's the first number?");
 let number1 = readline.question();
 
-console.log("What's the second number?");
+while (invalidNumber(number1)) {
+  prompt("Hmm that doesn't look like a valid number.");
+  number1 = readline.question();
+}
+
+prompt("What's the second number?");
 let number2 = readline.question();
 
-console.log("What operation would you like to perform?");
+while (invalidNumber(number2)) {
+  prompt("Hmm that doesn't look like a valid number.");
+  number2 = readline.question();
+}
+
+prompt("What operation would you like to perform?");
 let operation = readline.question();
 
+while (!['1', '2', '3', '4'].includes(operation)) {
+  prompt('Must choose 1, 2, 3, or 4');
+  operation = readline.question();
+}
 
 let output;
 
-if (operation === '1') {
-  output = Number(number1) + Number(number2);
-}
-else if (operation === '2') {
-  output = Number(number1) - Number(number2);
-}
-else if (operation === '3') {
-  output = Number(number1) * Number(number2);
-}
-else if (operation === '4') {
-  output = Number(number1) / Number(number2);
-}
+switch (operation) {
+  case '1':
+    output = Number(number1) + Number(number2);
+    break;
 
+  case '2':
+    output = Number(number1) - Number(number2);
+    break;
 
+  case '3':
+    output = Number(number1) * Number(number2);
+    break;
+
+  case '4':
+    output = Number(number1) / Number(number2);
+    break;
+}
 
 console.log(`The result is ${output}`);
-
-
-
-
