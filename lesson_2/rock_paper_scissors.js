56ddce7e096f780d6fe8a6cc6eb44072a1bfed51
Zmahl <zachmahl@udel.edu>
2022-10-11 22:02:49 -0400

    rock paper scissors done

--- lesson_2/rock_paper_scissors.js
+++ lesson_2/rock_paper_scissors.js
@@ -1,19 +1,52 @@
 const readline = require('readline-sync');
-const VALID_CHOICES = ['rock', 'paper', 'scissors'];
+const VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+const WINNING_COMBOS = {
+  rock: ['scissors', 'lizard'],
+  paper: ['rock', 'spock'],
+  scissors: ['paper', 'lizard'],
+  lizard: ['paper', 'spock'],
+  spock: ['rock', 'scissors']
+};
 
 function prompt(message) {
   console.log(`=> ${message}`);
 }
 
-prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
-let choice = readline.question();
-
-while (!VALID_CHOICES.includes(choice)) {
-  prompt("That's not a valid choice");
-  choice = readline.question();
+function playerWins(choice, computerChoice) {
+  return WINNING_COMBOS[choice].includes(computerChoice);
 }
 
-let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
-let computerChoice = VALID_CHOICES[randomIndex];
+function displayWinner(choice, computerChoice) {
+  if (playerWins(choice, computerChoice)) {
+    prompt('You win!');
+  } else if (choice === computerChoice) {
+    prompt("It's a tie!");
+  } else {
+    prompt("Computer wins!");
+  }
+}
 
-prompt(`You chose ${choice}, computer chose ${computerChoice}`);
+let answer = 'y';
+
+while (answer[0] === 'y') {
+
+  prompt(`Choose one: ${Object.keys(VALID_CHOICES).join(', ')}`);
+  let choice = readline.question();
+  
+  while (!VALID_CHOICES.includes(choice)) {
+    prompt("That's not a valid choice");
+    choice = readline.question();
+  }
+
+  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
+  let computerChoice = VALID_CHOICES[randomIndex];
+
+  displayWinner(choice, computerChoice);
+
+  prompt("Do you want to play again (y/n)?");
+  answer = readline.question().toLowerCase();
+  while (answer[0] !== 'n' && answer[0] !== 'y') {
+    prompt('Please enter "y" or "n".');
+    answer = readline.question().toLowerCase();
+  }
+}
