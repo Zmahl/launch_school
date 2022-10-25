2c93aea4e91c8b03cf10c2f82abdeedb17943662
Zmahl <zachmahl@udel.edu>
2022-10-24 20:05:10 -0400

    tic tac toe part 4 complete

--- lesson_6/tictactoe.js
+++ lesson_6/tictactoe.js
@@ -9,6 +9,10 @@ function prompt(message) {
 }
 
 function displayBoard(board) {
+  console.clear();
+
+  console.log(`You are ${HUMAN_MARKER}. Computer is ${COMPUTER_MARKER}`);
+
   console.log('');
   console.log('     |     |');
   console.log(`  ${board['1']}  |  ${board['2']}  |  ${board['3']}`);
@@ -22,6 +26,45 @@ function displayBoard(board) {
   console.log(`  ${board['7']}  |  ${board['8']}  |  ${board['9']}`);
   console.log('     |     |');
   console.log('');
+}
+
+function boardFull(board) {
+  return emptySquares(board).length === 0;
+}
+
+function someoneWon(board) {
+  //!! transforms truthy / falsy values to true / false
+  return !!detectWinner(board);
+}
+
+function detectWinner(board) {
+  let winningLines = [
+    [1, 2, 3], [4, 5, 6], [7, 8, 9], // rows
+    [1, 4, 7], [2, 5, 8], [3, 6, 9], // columns
+    [1, 5, 9], [3, 5, 7]             // diagonals
+  ]
+
+  for (let line = 0; line < winningLines.length; line++) {
+    //this syntax is 'destucturing' array into these 3 values
+    let [ sq1, sq2, sq3 ] = winningLines[line];
+  
+    if (
+      board[sq1] === HUMAN_MARKER &&
+      board[sq2] === HUMAN_MARKER &&
+      board[sq3] === HUMAN_MARKER
+    ) {
+      return 'Player';
+    }
+    else if (
+      board[sq1] === COMPUTER_MARKER &&
+      board[sq2] === COMPUTER_MARKER &&
+      board[sq3] === COMPUTER_MARKER
+    ) {
+      return 'Computer';
+    }
+  }
+
+  return null;
 }
 
 function initializeBoard() {
@@ -61,11 +104,35 @@ function computerChoosesSquare(board) {
   board[square] = COMPUTER_MARKER;
 }
 
-//Create empty board for the start of the game
-let board = initializeBoard();
-displayBoard(board);
+while (true) {
+  //Create empty board for the start of the game
+  let board = initializeBoard();
 
-playerChoosesSquare(board);
-computerChoosesSquare(board);
+  while (true)  {
 
-displayBoard(board);
+    displayBoard(board);
+
+    playerChoosesSquare(board);
+    //Use this if call to prevent "double winner" condition
+    if (someoneWon(board) || boardFull(board)) break;
+    computerChoosesSquare(board);
+
+    if (someoneWon(board) || boardFull(board)) break;
+  }
+
+  displayBoard(board);
+
+  if (someoneWon(board)) {
+    prompt(`${detectWinner(board)} won!`);
+  }
+
+  else {
+    prompt("It's a tie!");
+  }
+
+  prompt('Play again? (y or n)');
+  let answer = readline.question().toLowerCase()[0];
+  if (answer !== 'y') break;
+}
+
+prompt('Thanks for playing Tic Tac Toe');
