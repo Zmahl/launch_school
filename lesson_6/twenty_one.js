c6b9bc02a01fb12079bc7aa328eb2d5009f1f30d
Zmahl <zachmahl@udel.edu>
2022-10-29 18:26:28 -0400

    started 21

---
+++ lesson_6/twenty_one.js
@@ -0,0 +1,38 @@
+const readline = require('readline-sync');
+
+const SUIT = ["hearts", "clubs", "spades", "diamonds"];
+const CARDS = {
+  '1': 1,
+  '2': 2,
+  '3': 3,
+  '4': 4,
+  '5': 5,
+  '6': 6,
+  '7': 7,
+  '8': 8,
+  '9': 9,
+  '10': 10,
+  'J': 10,
+  'Q': 10,
+  'K': 10,
+  'A': 11,
+}
+
+const ACE_VALUES = {
+  '1': 1,
+  '11': 11
+}
+
+
+function initializeDeck() {
+  let deck;
+  return deck;
+}
+
+function drawCard() {
+
+}
+
+function dealCards() {
+
+}
