c5ef4c6825d3afc361c2f91bf65023d08e5a277e
Zach <zachmahl@udel.edu>
2023-01-09 23:18:22 -0500

    started ttt

---
+++ js120/lesson_5/oo_ttt.js
@@ -0,0 +1,145 @@
+class Square {
+  //Static properties are just properties that belong to the class
+  static UNUSED_SQUARE = " ";
+  static HUMAN_MARKER = "X";
+  static COMPUTER_MARKER = "O";
+
+  constructor(marker = Square.UNUSED_SQUARE) {
+    this.marker = marker;
+  }
+  //Override the toString method within the prototype chain
+  toString() {
+    return `${this.marker}`;
+  }
+}
+
+class Board {
+  constructor() {
+    this.squares = {};
+    for (let counter = 1; counter <= 9; ++counter) {
+      this.squared[String(counter)] = new Square();
+    }
+  }
+
+  display() {
+    console.log("");
+    console.log("     |     |");
+    console.log(`  ${this.squares["1"]}  |  ${this.squares["2"]}  |  ${this.squares["3"]}  `);
+    console.log("     |     |");
+    console.log("-----+-----+-----");
+    console.log("     |     |");
+    console.log(`  ${this.squares["4"]}  |  ${this.squares["5"]}  |  ${this.squares["6"]}  `);
+    console.log("     |     |");
+    console.log("-----+-----+-----");
+    console.log("     |     |");
+    console.log(`  ${this.squares["7"]}  |  ${this.squares["8"]}  |  ${this.squares["9"]}  `);
+    console.log("     |     |");
+    console.log("");
+  }
+}
+
+class Row {
+  constructor() {
+    //STUB
+    // We need some way to identify a row of 3 squares
+  }
+}
+
+class Marker {
+  constructor() {
+    //STUB
+    // A marker is something that represents a player's "piece" on the board.
+  }
+}
+
+class Player {
+  constructor() {
+    //STUB
+    // maybe a "marker" to keep track of this player's symbol (i.e., 'X' or 'O')
+  }
+
+  mark() {
+    //STUB
+    // We need a way to mark the board with this player's marker.
+    // How do we access the board?
+  }
+
+  play() {
+    //STUB
+    // We need a way for each player to play the game.
+    // Do we need access to the board?
+  }
+}
+
+class Human extends Player {
+  constructor() {
+    //STUB
+  }
+}
+
+class Computer extends Player {
+  constructor() {
+    //STUB
+  }
+}
+
+class TTTGame {
+  // omitted code
+  constructor() {
+    this.board = new Board();
+  }
+
+  play() {
+    //SPIKE
+    this.displayWelcomeMessage();
+
+    while (true) {
+      this.board.display();
+
+      this.firstPlayerMoves();
+      if (this.gameOver()) break;
+
+      this.secondPlayerMoves();
+      if (this.gameOver()) break;
+      break;
+    }
+
+    this.displayResults();
+    this.displayGoodbyeMessage();
+    
+  }
+
+  displayWelcomeMessage() {
+    console.log("Welcome to Tic Tac Toe!");
+  }
+
+  displayGoodbyeMessage() {
+    console.log("Thanks for playing TIc Tac Toe! Goodbye!");
+  }
+
+  displayResults() {
+    //STUB
+    // show the results of this game (win, lose, tie)
+  }
+
+
+
+  firstPlayerMoves() {
+    //STUB
+    // the first player makes a move
+  }
+
+  secondPlayerMoves() {
+    //STUB
+    // the second player makes a move
+  }
+
+  gameOver() {
+    //STUB
+    return false;
+  }
+}
+
+
+let game = new TTTGame();
+game.play();
