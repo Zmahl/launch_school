d494945e73aa8ab2ef907d02320f95b78b47c877
Zach <zachmahl@udel.edu>
2023-01-24 23:56:37 -0500

    almost done 21

--- js120/lesson_5/oo_twenty_one.js
+++ js120/lesson_5/oo_twenty_one.js
@@ -1,6 +1,8 @@
+const readline = require('readline-sync');
+
 class Card {
   static SUITS = ["Hearts", "Diamonds", "Spades", "Clubs"];
-  static CARD_TYPES = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+  static CARD_TYPES = ["2", "3", "4", "5", "6", "7", "8", "9", "10",
                       "Jack", "Queen", "King", "Ace"];
 
   constructor(suit, rank) {
@@ -9,6 +11,14 @@ class Card {
     // Rank? Suit? Points?
     this.suit = suit;
     this.rank = rank;
+  }
+
+  getSuit() {
+    return this.suit;
+  }
+
+  getRank() {
+    return this.rank;
   }
 }
 
@@ -30,9 +40,9 @@ class Deck {
   deal() {
     //STUB
     // does the dealer or the deck deal?
-    let index = () => Math.floor(Math.random() * this.cards.length);
-
-    let drawnCard = this.deck.splice(index, 1);
+    let index =  Math.floor(Math.random() * this.cards.length);
+    //Splice will return item as an array, so [0] is needed to remove it
+    let drawnCard = this.cards.splice(index, 1)[0];
 
     return drawnCard;
   }
@@ -49,29 +59,33 @@ class Participant {
 
   reset() {
     this.cardTotal = 0;
-    this.hand = {};
+    this.hand = [];
     this.bust = false;
   }
 
-  hit() {
-    this.hand.push(Deck.deal());
+  hit(deck) {
+    let drawnCard = deck.deal();
+    this.hand.push(drawnCard);
   }
 
   stay() {
     console.log(`${typeof(this)} stays. The final hand is ${this.score}`);
   }
 
-  isBusted() {
+  isBusted(bustLimit) {
     //STUB
-    if (this.cardTotal > 21) {
+    if (this.getScore() > bustLimit) {
       this.bust = true;
     }
-
     return this.bust;
   }
 
-  score(newCard) {
-    this.score_+= newCard.rank;
+  setScore(cardTotal) {
+    this.cardTotal = cardTotal;
+  }
+  
+  getScore() {
+    return this.cardTotal;
   }
 }
 
@@ -83,79 +97,209 @@ class Player extends Participant {
     super();
   }
 
-
+  displayHand() {
+    let str = "";
+    for (let i = 0; i < this.hand.length; i++) {
+      if (i === 0) str += `${this.hand[i].getRank()} of ${this.hand[i].getSuit()}`
+      else str += `, ${this.hand[i].getRank()} of ${this.hand[i].getSuit()}`
+    }
+    return str;
+  }
 
 }
 
 class Dealer extends Participant {
   // Very similar to a Player; do we need this?
+  static DEALER_LIMIT = 17;
 
   constructor() {
-    //STUB
-    // What sort of state does a dealer need?
-    // Score? Hand? Deck of cards? Bow tie?
     super();
   }
 
   hide() {
     //STUB
+    return "Facedown"
   }
 
   reveal() {
     //STUB
   }
 
-  deal() {
-    //STUB
-    // does the dealer or the deck deal?
+  displayHand() {
+    let str = this.hide();
+    for (let i = 1; i < this.hand.length; i++) {
+      str += `, ${this.hand[i].getRank()} of ${this.hand[i].getSuit()}`
+    }
+    return str;
   }
+
+  displayFinalHand() {
+    let str = "";
+    for (let i = 0; i < this.hand.length; i++) {
+      if (i === 0) str += `${this.hand[i].getRank()} of ${this.hand[i].getSuit()}`
+      else str += `, ${this.hand[i].getRank()} of ${this.hand[i].getSuit()}`
+    }
+    return str;
+  }
+
 }
 
 class TwentyOneGame {
+  static BUST_LIMIT = 21;
+  static STARTING_HAND_SIZE = 2;
+  static ACE_VALUE = 11;
+  static FACE_VALUE = 10;
+  static CHANGE_ACE_VALUE = 10;
+
   constructor() {
     //STUB
     // What sort of state does the game need?
     // A deck? Two participants?
+    this.player = new Player();
+    this.dealer = new Dealer();
+    this.deck = new Deck();
   }
 
   start() {
     //SPIKE
     this.displayWelcomeMessage();
+    this.displayRules();
     this.dealCards();
     this.showCards();
     this.playerTurn();
-    this.dealerTurn();
+    if(!this.checkPlayerBust()) {
+      this.dealerTurn();
+    }
+    this.displayFinalHands();
     this.displayResult();
-    this.displayGoodbyeMessage();
+    this.displayGoodbyeMessage();  
   }
 
   dealCards() {
     //STUB
-    
+    for (let i = 0; i < TwentyOneGame.STARTING_HAND_SIZE; i++) {
+      this.player.hit(this.deck);
+      this.dealer.hit(this.deck);
+    }  
   }
 
   showCards() {
     //STUB
+    console.log(`Player Starting Hand: ${this.player.displayHand()}`);
+    console.log(`Dealer Starting Hand: ${this.dealer.displayHand()}`);
+    console.log();
+    readline.question("Press any key to continue...")
+    console.clear();
   }
 
   playerTurn() {
     //STUB
+    let answer;
+    this.scoreCards(this.player);
+    while (!this.player.isBusted(TwentyOneGame.BUST_LIMIT)) {
+      console.log(`Current hand: ${this.player.displayHand()}`);
+      console.log("Would you like to hit or stay?");
+      answer = readline.question();
+      if (this.isPlayerHit(answer)) {
+        this.player.hit(this.deck);
+        this.scoreCards(this.player);
+      }
+
+      else break;
+    }
+    this.displayEndOfTurn(this.player);
+    readline.question("Press any key to continue...");
   }
 
   dealerTurn() {
     //STUB
+    this.scoreCards(this.dealer);
+    while (this.dealer.getScore() > TwentyOneGame.DEALER_LIMIT) {
+      this.dealer.hit(this.deck);
+      this.scoreCards(this.dealer);
+    }
   }
 
   displayWelcomeMessage() {
     //STUB
+    console.clear();
+    console.log("Welcome to Twenty-One!");
+  }
+
+  displayRules() {
+    console.log(`Draw cards to get as close as possible to ${TwentyOneGame.BUST_LIMIT}. But make sure not to go over!`);
+    console.log("Use 'hit' to draw additional cards, and 'stay' when you are happy with your hand.");
+    console.log();
+    readline.question("Press any key to play...");
+    console.clear();
   }
 
   displayGoodbyeMessage() {
     //STUB
+    console.log("Thanks for playing Twenty-One!");
   }
 
   displayResult() {
     //STUB
+    let winner = this.getWinner();
+    console.log(`${winner} wins the hand!`);
+  }
+
+  getWinner() {
+    if (this.player.isBusted) return "Dealer"
+    if (this.dealer.isBusted) return "Player"
+    if (this.player.getScore() > this.dealer.getScore()) return "Player";
+    else return "Dealer";
+  }
+
+  scoreCards(participant) {
+      let total = 0;
+      let cardValues = participant.hand.map((card) => card.rank);
+    
+      cardValues.forEach((rank) => {
+        if (rank === 'Ace') {
+          total += TwentyOneGame.ACE_VALUE;
+        } else if (['Jack', 'Queen', 'King'].includes(rank)) {
+          total += TwentyOneGame.FACE_VALUE;
+        } else {
+          total += Number(rank);
+        }
+      });
+    
+      cardValues
+        .filter((number) => number === 'A')
+        .forEach((_) => {
+          if (total > TwentyOneGame.BUST_LIMIT) total -= TwentyOneGame.CHANGE_ACE_VALUE;
+        });
+    
+      participant.setScore(total);
+  }
+
+  isPlayerHit(answer) {
+    //case for continuing players turn
+    while (true) {
+      if (answer.toLowerCase()[0] === 'h') return true
+      else if (answer.toLowerCase()[0] === 's') return false;
+      else {
+        console.log("Not a valid response")
+        answer = readline.question();
+      }
+    }
+  }
+
+  displayEndOfTurn(participant) {
+    console.log(`Final Hand is ${participant.displayHand()} with a score of ${participant.getScore()}`);
+    console.log();
+  }
+
+  checkPlayerBust() {
+    if (this.player.isBusted()) return true;
+    return false;
+  }
+
+  displayFinalHands() {
+    console.log(`Player Final Hand: ${this.player.displayHand()}`);
+    console.log(`Dealer Final Hand: ${this.dealer.displayFinalHand()}`);
   }
 }
 
